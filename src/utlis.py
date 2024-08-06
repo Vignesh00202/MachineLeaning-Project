b963fa6c7f9a4aa54b31836241cc447065421d30
Vignesh00202 <vigneshmurugiah2004@gmail.com>
2024-08-06 23:55:50 +0530

    HyperParamete Tuning

--- src/utlis.py
+++ src/utlis.py
@@ -5,6 +5,7 @@ import pandas as pd
 import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -16,14 +17,25 @@ def save_object(file_path,obj):
         raise CustomException(e,sys)
     
 
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
-        report={}
-        for name, model in models.items():
+        report = {}
+        for model_name, model in models.items():
+            param = params.get(model_name, {})
+            gs = GridSearchCV(model, param, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
-            report[name] = test_model_score
+
+            report[model_name] = test_model_score
         return report
+
     except Exception as e:
         raise CustomException(e,sys)
